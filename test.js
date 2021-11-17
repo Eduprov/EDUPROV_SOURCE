3b2b64444c4d6a2e9a65f3490e69e012c27c472a
thephotogenicbug <navinkumarachari@gmail.com>
2021-11-17 16:26:56 +0530

    EDUPROV : Add Course Data inside Tab's and Add Testimonials and Add some more course tags inside mega menu
    Signed-off-by: thephotogenicbug <navinkumarachari@gmail.com>

--- test.js
+++ test.js
@@ -93,3 +93,16 @@ function showSuggestions(list) {
   }
   suggBox.innerHTML = listData;
 }
+
+$(document).ready(function () {
+  $("#testimonial-slider").owlCarousel({
+    items: 1,
+    itemsDesktop: [1000, 1],
+    itemsDesktopSmall: [979, 1],
+    itemsTablet: [768, 1],
+    pagination: false,
+    navigation: true,
+    navigationText: ["", ""],
+    autoPlay: true,
+  });
+});
