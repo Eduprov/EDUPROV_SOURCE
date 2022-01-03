b3b957b9e6208add565802c52a6de72093f882c8
thephotogenicbug <navinkumarachari@gmail.com>
2022-01-03 12:52:53 +0530

    03-01-2021 : display social media icons in mobile
     and cleanup some css

--- test.js
+++ test.js
@@ -56,44 +56,6 @@ window.onresize = function () {
   }
 };
 
-// Search functionality
-const searchWrapper = document.querySelector(".search-input");
-const inputBox = searchWrapper.querySelector("input");
-const suggBox = searchWrapper.querySelector(".autocom-box");
-
-// user input
-inputBox.onkeyup = (e) => {
-  // console.log(e.target.value);
-  let userData = e.target.value; // user entered data
-  let emptyArray = [];
-  if (userData) {
-    emptyArray = suggestions.filter((data) => {
-      // filter array and user char to lowercase and return only those data which user entered
-      return data.toLowerCase().startsWith(userData.toLowerCase());
-    });
-    // console.log(emptyArray);
-    emptyArray = emptyArray.map((data) => {
-      return (data = "<li>" + data + "</li>");
-    });
-    console.log(emptyArray);
-    searchWrapper.classList.add("active"); // show autocomplete boc
-  } else {
-    searchWrapper.classList.remove("active"); // hide autocomplete box
-  }
-  showSuggestions(emptyArray);
-};
-
-function showSuggestions(list) {
-  let listData;
-  if (!list.length) {
-    userValue = inputBox.value;
-    listData = "<li>" + userValue + "</li>";
-  } else {
-    listData = list.join("");
-  }
-  suggBox.innerHTML = listData;
-}
-
 $(document).ready(function () {
   $("#testimonial-slider").owlCarousel({
     items: 1,
